ff76e097963a7d5ac45972e8650be17725f23eed
Ana Sánchez <anithap_745@hotmail.com>
2018-11-13 22:46:17 -0700

    Add project files.

---
+++ AppDAE2/Areas/General/Controllers/FicCatEdificiosController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppDAE2.Areas.General.Services;
+using AppDAE2.Models;
+
+namespace AppDAE2.Areas.General.Controllers
+{
+    [Area("General")]
+    public class FicCatEdificiosController : Controller
+    {
+        FicSrcCatEdificiosList FicService;
+        List<eva_cat_edificios> FicLista;
+
+        public FicCatEdificiosController() {
+            
+         }
+        public IActionResult FicViCatEdificiosList()
+        {
+            try
+            {
+                FicService = new FicSrcCatEdificiosList();
+                FicLista = FicService.FicGetListCatEdificios().Result;
+                ViewBag.Title = "Catalogo de edificios";
+                return View(FicLista);
+                   
+            }
+            catch (Exception e) {
+
+                throw;
+                
+            }
+        }
+
+    }
+}
---
+++ AppDAE2/Areas/General/Services/FicSrcCatEdificiosList.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http.Headers;
+using AppDAE2.Models;
+using Newtonsoft.Json;
+
+namespace AppDAE2.Areas.General.Services
+{
+    public class FicSrcCatEdificiosList
+    {
+        HttpClient FiClient;
+
+
+        public FicSrcCatEdificiosList()
+        {
+            this.FiClient = new HttpClient();
+            this.FiClient.BaseAddress = new Uri("http://localhost:51049/");
+            this.FiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+        public async Task<List<eva_cat_edificios>> FicGetListCatEdificios()
+        {
+            HttpResponseMessage FicResponse = await this.FiClient.GetAsync("api/GetEdificios");
+            if (FicResponse.IsSuccessStatusCode)
+            {
+                var FicRespuesta = await FicResponse.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<List<eva_cat_edificios>>(FicRespuesta);
+
+            }
+            return new List<eva_cat_edificios>();
+        }
+        
+    }
+}
